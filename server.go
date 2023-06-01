d2861163c7d9964dfbfcf15f4cfce64aad90df03
Tanaka <alongthejourneyahead@gmail.com>
2023-06-01 22:07:07 +0100

    changed system message

--- server.go
+++ server.go
@@ -78,7 +78,7 @@ func processQuery(query string) string {
 
 	//"gpt-3.5-turbo"
 
-	inp := Input{client: client, prompt: query + "\n", model: "gpt-4", temperature: 0.9, maxTokens: 1500, systemMessage: `You are a gardening agent, you do not respond with any response that starts with "As an AI model". Use a friendly tone`}
+	inp := Input{client: client, prompt: query + "\n", model: "gpt-3.5-turbo", temperature: 0.9, maxTokens: 1500, systemMessage: `You are a ardening helper, you do not respond with any response that starts with "As an AI model".  You will only answer questions about gardening, allotmenting and any related topics to do with plant growing. If you recieve a question thats not related to gardening or allotmenting, you will politely decline to answer and respond with "I cannot answer that this question because it's not gardening related".  Use a friendly tone`}
 
 	var (
 		str string
