b358ca03dafeea9925784faa783680356e1e72a7
Tanaka <alongthejourneyahead@gmail.com>
2023-11-18 22:19:11 +0000

    cleaned up codebase

--- main.go
+++ main.go
@@ -18,27 +18,9 @@ type Input struct {
 	prompt, model, systemMessage string
 	temperature                  float32
 	maxTokens                    int
-	res                          string // maybe this could be a generic so that it can be both a slice, string or a null
-}
-
-type Plugin struct {
 }
 
 func main() {
-
-	/*
-
-			r := mux.NewRouter()
-		r.HandleFunc("/", handleRequest).Methods("POST")
-
-		// Enable CORS
-		corsHandler := cors.Default().Handler(r)
-
-		log.Println("Server listening on port 8080...")
-		log.Fatal(http.ListenAndServe(":8080", corsHandler))
-
-	*/
-
 	inp := Input{client: getClient(), prompt: "Create questions for a job interview for a financial accounts manager in the uk" + "\n", model: "gpt-3.5-turbo-0613", temperature: 0.8, maxTokens: 1000, systemMessage: `Don't make assumptions about what values to plug into functions. Ask for clarification if a user request is ambiguous`}
 
 	_, err, thing := inp.getChatStreamResponse()
@@ -47,74 +29,6 @@ func main() {
 	if err != nil {
 		log.Println(err, "error from the model")
 	}
-
-	//log.Println("answer: " + thing)
-
-	// this creates files
-	/*
-		// check if output exist and if it does it gets moved to history under a unique name
-		err = ioutil.WriteFile("output.txt", []byte(str), 0644)
-		if err != nil {
-			panic(err)
-		}
-
-	*/
-
-	/*
-
-
-		fmt.Printf("HTML file %s written successfully\n", fileName)
-
-	*/
-
-	/*
-		//i := interpreter("what is the best way to make a pizza?")
-		//log.Println(i)
-		//x := getResponse(i)
-		//log.Println(x)
-
-		instruct := `Provide a message for encouragement to create a positive state of mind or feeling of better well-being.`
-		plugin := `**&timer: every other day at 9:20`
-
-		p := instruct + plugin
-
-		var f = func(s string) string {
-
-			// this should be in a looped map
-			if filterString(s, "**&timer:") {
-				// run the plugin
-
-			}
-
-			return s
-		}
-
-		// returns the string without the plugin key
-		var k = func(s string) string {
-
-			if String(s, "**&") {
-				// remove the plugin key from the string and what ever text is joining it
-			}
-
-			return s
-		}
-
-		runPlugins := f(p)
-		res := runPolicies(k(p), bool(true))
-
-		// handling the preprogrammed plugins
-		// todo: map should be used to store all the plugin keys such as **&timer:
-
-		_ = res
-
-		log.Println(res)
-
-		// sending sms
-		//sendSMS(os.Getenv("TWILIO_PHONE_FROM"), os.Getenv("TWILIO_PHONE_TO"), quote)
-
-
-	*/
-
 }
 
 func getClient() *openai.Client {
@@ -129,7 +43,6 @@ func getClient() *openai.Client {
 }
 
 func (inp Input) getChatStreamResponse() (string, error, string) {
-
 	var array = []openai.ChatCompletionMessage{
 		{
 			Role:    "system",
@@ -206,39 +119,4 @@ func (inp Input) getChatStreamResponse() (string, error, string) {
 	log.Println(res)
 
 	return buffer.String(), nil, res
-}
-
-func getStreamResponse(prompt string, g *openai.Client) (string, error) {
-	request := openai.CompletionRequest{
-		Model:     "text-ada-001",
-		MaxTokens: 500,
-		Prompt:    prompt,
-		Stream:    true,
-		//Stop:            []string{"human:", "ai:"},
-		//Temperature:     0,
-		//TopP:            1,
-		//PresencePenalty: 0.6,
-	}
-
-	stream, err := g.CreateCompletionStream(context.Background(), request)
-	if err != nil {
-		return "", err
-	}
-	defer stream.Close()
-
-	var buffer strings.Builder
-	for {
-		response, err := stream.Recv()
-		if err != nil {
-			return "", err
-		}
-
-		buffer.WriteString(response.Choices[0].Text)
-
-		if response.Choices[0].FinishReason != "" {
-			break
-		}
-	}
-
-	return buffer.String(), nil
 }
